7ddf6f99e8f3757a5ad6a664bdf4678303e024fd
y_uuki <yuki.tsubo@gmail.com>
2013-04-22 17:04:50 +0900

    Add Minilla first set

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+ModelValidator - It's new $module
+
+# SYNOPSIS
+
+    use ModelValidator;
+
+# DESCRIPTION
+
+ModelValidator is ...
+
+# LICENSE
+
+Copyright (C) y\_uuki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+y\_uuki <yuki.tsubo@gmail.com>
---
+++ lib/ModelValidator.pm
@@ -0,0 +1,36 @@
+package ModelValidator;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+ModelValidator - It's new $module
+
+=head1 SYNOPSIS
+
+    use ModelValidator;
+
+=head1 DESCRIPTION
+
+ModelValidator is ...
+
+=head1 LICENSE
+
+Copyright (C) y_uuki
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+y_uuki E<lt>yuki.tsubo@gmail.comE<gt>
+
